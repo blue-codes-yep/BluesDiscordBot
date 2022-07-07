ce46063957c08daf908c8399f71e98ff74abbbca
linzblue1 <linzblue5@gmail.com>
2022-07-07 00:12:38 -0500

    Welcome member to the server

--- bot.py
+++ bot.py
@@ -5,16 +5,15 @@ class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as', self.user)
 
-    async def on_message(self, message):
-        # don't respond to ourselves
-        if message.author == self.user:
-            return
-
-        if message.content == 'ping':
-            await message.channel.send('pong')
+    async def on_member_join(self, member):
+        guild = member.guild
+        if guild.system_channel is not None:
+            to_send = f'Welcome {member.mention} to the best {guild.name}!'
+            await guild.system_channel.send(to_send)
 
 
 intents = discord.Intents.default()
-intents.message_content = True
+intents.members = True
+
 client = MyClient(intents=intents)
-client.run('insert bot token')
+client.run('NzYwMzA0NDMxNDA4NjExMzk4.G9HP_b.AAguTjsC-EBvBDozA1v7BAEnGcfEGPGlMWjEA4')
