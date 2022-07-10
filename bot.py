30f04afcf399d5fb8e31dc9ff17a4155d8fa6ef2
linzblue1 <linzblue5@gmail.com>
2022-07-09 22:06:03 -0500

    Basic refactor removing some imports

--- bot.py
+++ bot.py
@@ -1,7 +1,5 @@
 import discord
 from discord.ext import commands
-import events
-import asyncio
 
 
 class MyBot(commands.Bot):
@@ -16,4 +14,4 @@ intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 Bot = MyBot(intents=intents, command_prefix='!')
-Bot.run('token')
+Bot.run('NzYwMzA0NDMxNDA4NjExMzk4.GATbGO.ktvsY8n1xNGJ-G4hRStrTHLHv8pfwNZs-PyMco')
